91e48d6f8a754befabf0d857311976c1bf252ec3
Chen ZhiYong <chenzhiyong@innoplay.cn>
2017-11-01 16:31:27 +0800

    Initial commit

---
+++ HelloWorldTest.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+#coding=utf-8
+import math
+
+print("哈哈哈");
+# raw_input("\n\nPress the enter key to exit.");
+
+x=2
+y="b"
+print x,
+print x
+
+list = ['runoob', 786, 2.23, 'john', 70.2]
+tinylist = [123, 'john']
+
+print oct(10)
+
+# a=12
+# b=5
+# c=a//b
+# print c
+# print a%b ##余数
+# print a==b
+# print a>b
+# print a or b
+# print a and b
+
+
+# if b=="ccc":
+#
+#     print "相等"
+#
+# else:
+#     print "不相等"
+
+# for letter in 'Python':  # 第一个实例
+#     print '当前字母 :', letter
+
+# print r'我\000你'
+print "\baaaa"
+
+
+
+# print abs(-10)
+# print math.sqrt(4.2)
+
+# i=1
+# n=i
+# sum=0
+# while n<20:
+#     n+=1
+#     i = 2 ** (n - 1)
+#     sum=sum+i
+# print(sum)
+# print()
+
+# var1='Hello World'
+# print var1[:6]+'python'
+
+d = {
+    'Adam': 95,
+    'Lisa': 85,
+    'Bart': 59
+}
+print (d['Adam'])+5
---
+++ pythonDemo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from socket import *
+from time import ctime
+
+HOST = ''
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+
+
+while True:
+    print 'waiting for connection...'
+    tcpCliSock, addr = tcpSerSock.accept()
+    print '...connected from:', addr
+
+    while True:
+        data = tcpCliSock.recv(BUFSIZ)
+        if not data:
+            break
+        tcpCliSock.send('get your data:%s\n[%s]' % (data, ctime()))
+
+        tcpCliSock.close
+
+tcpSerSock.close
