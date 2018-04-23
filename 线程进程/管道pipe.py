d789a0ca79deab52bd9b30d3ff17d5a9ea8f4630
CrazyYong <504500534@qq.com>
2018-04-23 18:25:08 +0800

    多线程/进程

---
+++ 线程进程/管道pipe.py
@@ -0,0 +1,36 @@
+# -*- conding:UTF-8 -*-
+import multiprocessing
+import time
+def send_data(pipe,message):
+    pipe.send(message)
+
+def recv_data(pipe):
+    value=pipe.recv()
+    print('接收的一个值是%s'%value)
+
+if __name__=='__main__':
+    #Pipe里面的duplex默认的值True,全双工管道
+    pipe=multiprocessing.Pipe()
+    message='Python'
+    p1=multiprocessing.Process(target=send_data,args=(pipe[1],message))
+    p2=multiprocessing.Process(target=recv_data,args=(pipe[0],))
+    p1.start()
+    p2.start()
+
+
+
+#当duplex为false时,左边收,右边发
+# def send_data(pipe,message):
+#     pipe.send(message)
+#
+# def recv_data(pipe):
+#     value=pipe.recv()
+#     print('接收的一个值是%s'%value)
+#
+# if __name__=='__main__':
+#     pipe = multiprocessing.Pipe()
+#     message = 'Python'
+#     p1 = multiprocessing.Process(target=send_data, args=(pipe[0], message))
+#     p2 = multiprocessing.Process(target=recv_data, args=(pipe[1],))
+#     p1.start()
+#     p2.start()
